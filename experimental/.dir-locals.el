aac8f18d1295d851377603a11885b62203f09407
J David Smith <emallson@archlinux.us>
2015-04-22 16:20:35 -0400

    Got a minion detector (mostly) working

--- experimental/.dir-locals.el
+++ experimental/.dir-locals.el
@@ -2,5 +2,6 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((org-mode
+  (org-babel-use-quick-and-dirty-noweb-expansion . t)
   (pyvenv-activate . "./venv/")
   (org-src-preserve-indentation . t)))
